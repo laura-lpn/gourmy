28ff45f4198fed07ddc982b94ffaf1cd6bd7328a
laura_lpn <lauralepannetier@gmail.com>
2024-12-12 10:52:13 +0100

    chore: lint+ fixer

--- src/Controller/Api/Auth/LoginController.php
+++ src/Controller/Api/Auth/LoginController.php
@@ -46,28 +46,36 @@ class LoginController extends AbstractController
     public function resendConfirmationEmail(Request $request, UserRepository $userRepository): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
-        if (!isset($data['email'])) {
+        if (null === $data) {
+            return new JsonResponse(['message' => 'Données invalides. Veuillez envoyer une requête JSON valide.'], 400);
+        }
+        if (empty($data['email'])) {
             throw new BadRequestHttpException('L\'adresse email est obligatoire.');
         }
 
         $email = $data['email'];
-
         $user = $userRepository->findOneBy(['email' => $email]);
 
-        if ($user && !$user->isVerified()) {
-            try {
-                $this->emailVerifier->sendEmailConfirmation(
-                    'api_verify_email',
-                    $user,
-                    (new TemplatedEmail())
-                        ->from(new Address('gourmy@gmail.com', 'Gourmy'))
-                        ->to((string) $user->getEmail())
-                        ->subject('Confirmez votre adresse email')
-                        ->htmlTemplate('email/confirmation_email.html.twig')
-                );
-            } catch (\Exception $e) {
-                return new JsonResponse(['message' => 'Erreur lors de l\'envoi de l\'email de confirmation. Veuillez réessayer plus tard.'], 500);
-            }
+        if (!$user) {
+            return new JsonResponse(['message' => 'Si un utilisateur est trouvé, un email de confirmation sera envoyé.'], 200);
+        }
+
+        if ($user->isVerified()) {
+            return new JsonResponse(['message' => 'Cet utilisateur a déjà vérifié son adresse email.'], 200);
+        }
+
+        try {
+            $this->emailVerifier->sendEmailConfirmation(
+                'api_verify_email',
+                $user,
+                (new TemplatedEmail())
+                    ->from(new Address('gourmy@gmail.com', 'Gourmy'))
+                    ->to($user->getEmail())
+                    ->subject('Confirmez votre adresse email')
+                    ->htmlTemplate('email/confirmation_email.html.twig')
+            );
+        } catch (\Exception $e) {
+            return new JsonResponse(['message' => 'Erreur lors de l\'envoi de l\'email de confirmation. Veuillez réessayer plus tard.'], 500);
         }
 
         return new JsonResponse(['message' => 'Si un utilisateur est trouvé, un email de confirmation sera envoyé.'], 200);
