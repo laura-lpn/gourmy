1b63c2ab013557f2f107353e5ec9936e2cb14cae
laura-lpn <lauralepannetier@gmail.com>
2025-04-22 00:15:27 +0200

    feat: add Google Maps integration and enhance restaurant forms with MapPreview component

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Restaurant;
-use App\Form\CreateRestaurantType;
+use App\Form\RestaurantType;
+use App\Repository\RestaurantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class RestaurantController extends AbstractController
 {
@@ -23,61 +23,116 @@ class RestaurantController extends AbstractController
         return $this->render('restaurant/index.html.twig');
     }
 
-    #[Route('/restaurateur/mon-restaurant', name: 'app_restaurant')]
-    public function restaurant(): Response
-    {
-        if (!$this->getUser()) {
-            $this->addFlash('error', 'Vous devez être connecté pour accéder à cette page.');
-            return $this->redirectToRoute('app_login');
-        }
-        return $this->render('restaurant/restaurant_profile.html.twig');
-    }
-
     #[Route('/restaurateur/creer-un-restaurant', name: 'app_restaurant_create')]
-    public function createRestaurant(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    public function createRestaurant(Request $request, EntityManagerInterface $em): Response
     {
-        if (!$this->getUser()) {
+        $user = $this->getUser();
+        if (!$user) {
             $this->addFlash('error', 'Vous devez être connecté pour accéder à cette page.');
             return $this->redirectToRoute('app_login');
         }
+
         /** @var \App\Entity\User $user */
-        $user = $this->getUser();
-
-        if (in_array('ROLE_RESTAURATEUR', $user->getRoles())) {
-            return $this->redirectToRoute('app_restaurateur');
-        }
-
-        if ($user->getRestaurant()) {
-            return $this->redirectToRoute('app_restaurant');
+        if (in_array('ROLE_RESTAURATEUR', $user->getRoles()) && $user->getRestaurant()) {
+            return $this->redirectToRoute('app_restaurant_profile');
         }
 
         $restaurant = new Restaurant();
-        $form = $this->createForm(CreateRestaurantType::class, $restaurant);
+        $roles = array_unique(array_merge($user->getRoles(), ['ROLE_RESTAURATEUR']));
+        $user->setRoles($roles);
+
+        $form = $this->createForm(RestaurantType::class, $restaurant, [
+            'allow_file_upload' => true,
+            'csrf_protection' => true
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($restaurant);
+            $user->setRestaurant($restaurant);
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre demande de création de restaurant a bien été envoyée...');
+
+            return $this->redirectToRoute('app_restaurant_profile');
+        }
+
+        return $this->render('restaurant/restaurant_create.html.twig', [
+            'restaurantForm' => $form->createView(),
+            'restaurant' => $restaurant,
+            'google_maps_api_key' => $_ENV['GOOGLE_MAPS_API_KEY'],
+        ]);
+    }
+
+    #[Route('/restaurateur/mon-restaurant', name: 'app_restaurant_profile')]
+    public function restaurant(RestaurantRepository $restaurantRepository): Response
+    {
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'Vous devez être connecté pour accéder à cette page.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        if (!$user->getRestaurant()) {
+            $this->addFlash('error', 'Vous n\'avez pas de restaurant associé à votre compte.');
+            return $this->redirectToRoute('app_restaurant_create');
+        }
+
+        $restaurantid = $user->getRestaurant()->getId();
+
+        $restaurant = $restaurantRepository->find($restaurantid);
+
+        if (!$restaurant) {
+            $this->addFlash('error', 'Restaurant introuvable.');
+            return $this->redirectToRoute('app_restaurant_create');
+        }
+
+        $isValidated = $restaurant->isValided();
+        if (!$isValidated) {
+            $this->addFlash('warning', 'Votre restaurant n\'a pas encore été validé.');
+        }
+
+        return $this->render('restaurant/restaurant_profile.html.twig', [
+            'restaurant' => $restaurant,
+            'isValidated' => $isValidated,
+            'google_maps_api_key' => $_ENV['GOOGLE_MAPS_API_KEY'],
+        ]);
+    }
+
+    #[Route('/restaurateur/mon-restaurant/modifier', name: 'app_restaurant_edit')]
+    public function editRestaurant(Request $request, EntityManagerInterface $em): Response
+    {
+        if (!$this->getUser()) {
+            $this->addFlash('error', 'Vous devez être connecté pour accéder à cette page.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        if (!$user->getRestaurant()) {
+            $this->addFlash('error', 'Vous n\'avez pas de restaurant associé à votre compte.');
+            return $this->redirectToRoute('app_restaurant_create');
+        }
+
+        $restaurant = $user->getRestaurant();
+        $form = $this->createForm(RestaurantType::class, $restaurant);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $restaurant = $form->getData();
-            $restaurant->setValided(false);
-            $slug = $slugger->slug($restaurant->getName())->lower();
-            $restaurant->setSlug($slug);
-            $restaurant->setOwner($user);
+            $em->flush();
 
-            $roles = array_unique(array_merge($user->getRoles(), ['ROLE_RESTAURATEUR']));
-            $user->setRoles($roles);
-
-            $entityManager->persist($restaurant);
-            $user->setRestaurant($restaurant);
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Votre demande de création de restaurant a bien été envoyée. Elle sera validée après vérification des informations.');
-
-            return $this->redirectToRoute('app_restaurateur');
+            return $this->redirectToRoute('app_restaurant_profile');
         }
 
-        return $this->render('restaurant/restaurant_create.html.twig', [
-            'restaurantForm' => $form,
+        return $this->render('restaurant/restaurant_edit.html.twig', [
+            'restaurantForm' => $form->createView(),
+            'restaurant' => $restaurant,
+            'google_maps_api_key' => $_ENV['GOOGLE_MAPS_API_KEY'],
         ]);
     }
 }
