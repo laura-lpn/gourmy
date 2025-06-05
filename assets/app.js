ff67d3f04fcfdd23df35406a16a6e6368f1daf73
laura_lpn <lauralepannetier@gmail.com>
2025-06-05 17:47:44 +0200

    Chore : clean

--- assets/app.js
+++ assets/app.js
@@ -2,4 +2,4 @@ import './styles/app.css';
 import './bootstrap.js';
 import './webcomponents/reviews/RestaurantReviewManager.js';
 import './webcomponents/reviews/ReviewList.js';
-import './webcomponents/UserRoadtrips.js';
+import './webcomponents/roadtrips/UserRoadtrips.js';
